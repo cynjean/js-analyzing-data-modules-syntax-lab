337056308595492fe61c1f2a6f4f8c14435ad8ad
cynjean <cynthia.2056@yahoo.com>
2026-04-25 00:47:56 -0500

    Module 5 Lab Completed

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-
-
-
+require('datejs');
+//Create a function that merges all usernames into one
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  for(const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array]
+  }
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+return combinedObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
